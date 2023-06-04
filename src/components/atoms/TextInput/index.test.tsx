e75084e8f487a746382d6656870b8b1f425ff1d5
nakajima97 <kituneudon97@gmail.com>
2023-06-04 22:17:43 +0900

    #1 inputのテストコード修正

--- src/components/atoms/TextInput/index.test.tsx
+++ src/components/atoms/TextInput/index.test.tsx
@@ -7,10 +7,9 @@ import TextInput from './index';
 test('renders input element with correct value', () => {
   const testValue = 'Test Value';
   const handleChange = jest.fn();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+
   render(<TextInput value={testValue} onChange={handleChange} />);
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  const inputElement = screen.getByRole('textbox') as HTMLInputElement;
-  expect(inputElement.value).toBe(testValue);
+  const inputElement = screen.getByRole('textbox');
+  expect(inputElement).toBe(testValue);
 });
