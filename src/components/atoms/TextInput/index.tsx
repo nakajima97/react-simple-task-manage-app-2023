690f36c2dc7b7e2d1b9c36188cf3222396996883
nakajima97 <kituneudon97@gmail.com>
2023-06-08 08:50:24 +0900

    #1 inputのクラスを外から設定できるようにした

--- src/components/atoms/TextInput/index.tsx
+++ src/components/atoms/TextInput/index.tsx
@@ -3,10 +3,18 @@ import { FC } from 'react';
 type Props = {
   value: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 };
 
-const TextInput: FC<Props> = ({ value, onChange }) => {
-  return <input type="text" value={value} onChange={onChange} />;
+const TextInput: FC<Props> = ({ value, onChange, className }) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      className={className}
+    />
+  );
 };
 
 export default TextInput;
