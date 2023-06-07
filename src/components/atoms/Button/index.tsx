2e91faddb548ccb4b79d1bd99e8dc273d7645603
nakajima97 <kituneudon97@gmail.com>
2023-06-08 08:49:15 +0900

    #1 追加ボタンのデザイン調整

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -3,10 +3,15 @@ import React, { FC } from 'react';
 type Props = {
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
 };
 
-const Button: FC<Props> = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>;
+const Button: FC<Props> = ({ text, onClick, className }) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/molecules/AddButton/index.tsx
+++ src/components/molecules/AddButton/index.tsx
@@ -5,7 +5,13 @@ const AddButton: FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
   const onClick = () => {};
 
-  return <Button text="追加" onClick={onClick}></Button>;
+  return (
+    <Button
+      text="追加"
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    ></Button>
+  );
 };
 
 export default AddButton;
