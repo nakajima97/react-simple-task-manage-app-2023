862e55a5c9d4b9982111bd2d48fbce84e6eb0e92
nakajima97 <kituneudon97@gmail.com>
2023-06-09 08:18:49 +0900

    #3 タスク追加欄で文字を入力できるようにした

--- src/components/organisms/TaskAddition/index.tsx
+++ src/components/organisms/TaskAddition/index.tsx
@@ -1,14 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import TextInput from '../../atoms/TextInput';
 import AddButton from '../../molecules/AddButton';
 
 const TaskAddition: FC = () => {
+  const [newTask, setNewTask] = useState(String);
+
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const onChange = () => {};
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value);
+  };
 
   return (
     <div>
-      <TextInput value="aaa" onChange={onChange}></TextInput>
+      <TextInput value={newTask} onChange={onChange}></TextInput>
       <AddButton></AddButton>
     </div>
   );
